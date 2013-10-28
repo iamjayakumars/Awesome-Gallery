704e7527293516183dca04a104946d7063f08688
dannydes <desiradaniel2007@hotmail.com>
2013-10-28 20:53:52 +0100

    Should work for both array and NodeList photos and added 0 shortcut for last photo

--- swift_keys.js
+++ swift_keys.js
@@ -1,11 +1,17 @@
-AG.extend(function (exports) {
-  document.addEventListener( 'keydown', function (event) {
-    if (exports.dom.galleryActivated) {
-      exports.Switcher.goto( parseInt( String.fromCharCode( event.keyCode ) ) - 1 );
+AG.extend(function ( $ ) {
+  document.addEventListener( 'keydown', function ( event ) {
+    var keyCode, photosLength;
+    if ($.dom.galleryActivated) {
+      keyCode = parseInt( String.fromCharCode( event.keyCode ) );
+      photosLength = $.dom.images ? $.dom.images.length : $.photos.length;
+      
+      keyCode === 0 ?
+        $.Switcher.goto( photosLength - 1 ) :
+        $.Switcher.goto( keyCode - 1 );
       
       event.keyCode === 33 ?
-        exports.Switcher.prev() :
-        (event.keyCode === 34 ? exports.Switcher.next() : undefined);
+        $.Switcher.prev() :
+        (event.keyCode === 34 ? $.Switcher.next() : undefined);
     }
   });
 });
