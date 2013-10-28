ea5fa392d8759e8b6d956aadb81d5cbb986364a5
dannydes <desiradaniel2007@hotmail.com>
2013-10-28 20:24:28 +0100

    Plugin and internal API intersection! Woop woop!

--- js/viewer.js
+++ js/viewer.js
@@ -9,7 +9,7 @@
 
 var TRANSITION_CLASS = 'view';
 
-var currentImage, photoIndex, imageDescription, delay, photos, touchStartX, prev, next, startSlideshow, stopSlideshow,
+var photoIndex, delay, photos, touchStartX,
   
   Switcher = (function () {
     var interval,
@@ -42,20 +42,20 @@ var currentImage, photoIndex, imageDescription, delay, photos, touchStartX, prev
     };
 
     function changePhoto() {
-      currentImage.classList.remove( TRANSITION_CLASS );
+      ag.dom.image.classList.remove( TRANSITION_CLASS );
       setTimeout(function() {
-        currentImage.src = photos[ photoIndex ].src;
-        currentImage.onload = function () {
-          currentImage.classList.add( TRANSITION_CLASS );
-          if (imageDescription) {
-            imageDescription.textContent = photos[ photoSwitcher.getPhotoIndex() ].alt;
+        ag.dom.image.src = photos[ photoIndex ].src;
+        ag.dom.image.onload = function () {
+          ag.dom.image.classList.add( TRANSITION_CLASS );
+          if (ag.dom.description) {
+            ag.dom.description.textContent = photos[ photoSwitcher.getPhotoIndex() ].alt;
           }
         };
         
         //Work-around: WebKit and Blink do not display <img> alt text.
-        currentImage.onerror = function () {
-          imageDescription.textContent = (/WebKit/.test( navigator.userAgent ) ?
-                imageDescription.textContent : '') + ' Unable to fetch image!';
+        ag.dom.image.onerror = function () {
+          ag.dom.description.textContent = (/WebKit/.test( navigator.userAgent ) ?
+                ag.dom.description.textContent : '') + ' Unable to fetch image!';
         };
       }, delay );
     }
@@ -71,28 +71,29 @@ var currentImage, photoIndex, imageDescription, delay, photos, touchStartX, prev
     return Switcher;
   })(),
   
-  exports = {
+  ag = {
     Switcher: Switcher,
+    photos: [],
     dom: {
-      prev: prev,
-      next: next,
-      slideshowStart: startSlideshow,
-      slideshowEnd: stopSlideshow,
-      image: currentImage,
-      description: imageDescription,
-      images: !Array.isArray( photos ) ? photos : null,
+      prev: null,
+      next: null,
+      slideshowStart: null,
+      slideshowEnd: null,
+      image: null,
+      description: null,
+      images: null,
       galleryActivated: true
     }
   };
 
 function _activateGallery( gallery ) {
   document.documentElement.addEventListener( 'click', function ( event ) {
-    exports.dom.galleryActivated = (event.target === gallery);
+    ag.dom.galleryActivated = (event.target === gallery);
   });
 }
 
 function handleKeyDown( event ) {
-  if (exports.dom.galleryActivated) {
+  if (ag.dom.galleryActivated) {
     if (event.keyCode === 37) {
       Switcher.prev();
     } else if (event.keyCode === 39) {
@@ -135,9 +136,9 @@ function selectImage( image ) {
 window.AG = {};
 
 AG.init = function ( options ) {
-  currentImage = $( options.$image );
+  ag.dom.image = $( options.$image );
   if (options.$$images) {
-    photos = document.querySelectorAll( options.$$images );
+    ag.dom.images = photos = document.querySelectorAll( options.$$images );
     for (photoIndex = 0; photoIndex < photos.length; photoIndex++) {
       (function (i) {
         photos[photoIndex].addEventListener( 'click', function () {
@@ -147,44 +148,44 @@ AG.init = function ( options ) {
     }
     photoIndex = 0;
   } else {
-    photos = options.photos;
+    ag.photos = photos = options.photos;
   }
   
   selectImage( photos[0] );
-  currentImage.src = photos[0].src;
-  currentImage.classList.add( TRANSITION_CLASS );
+  ag.dom.image.src = photos[0].src;
+  ag.dom.image.classList.add( TRANSITION_CLASS );
   
   if (options.$description) {
-    imageDescription = $( options.$description );
+    ag.dom.description = $( options.$description );
   }
 
   delay = options.delay ?
     options.delay :
     (function () {
-      var transitionDuration = getComputedStyle( currentImage ).transitionDuration;
+      var transitionDuration = getComputedStyle( ag.dom.image ).transitionDuration;
       return parseFloat( transitionDuration.substring( 0, transitionDuration.length - 1 ) ) * 1000;
     })();
 
-  currentImage.addEventListener( 'click', Switcher.next );
+  ag.dom.image.addEventListener( 'click', Switcher.next );
   document.addEventListener( 'keydown', handleKeyDown );
-  currentImage.addEventListener( 'touchstart', handleTouchStart );
-  currentImage.addEventListener( 'touchend', handleTouchEnd );
-  prev = registerButtonClick( 'prev', options.$prev );
-  next = registerButtonClick( 'next', options.$next );
-  startSlideshow = registerButtonClick( 'slideshow', options.$slideshow );
-  stopSlideshow = registerButtonClick( 'stopslideshow', options.$stopSlideshow );
+  ag.dom.image.addEventListener( 'touchstart', handleTouchStart );
+  ag.dom.image.addEventListener( 'touchend', handleTouchEnd );
+  ag.dom.prev = registerButtonClick( 'prev', options.$prev );
+  ag.dom.next = registerButtonClick( 'next', options.$next );
+  ag.dom.slideshowStart = registerButtonClick( 'slideshow', options.$slideshow );
+  ag.dom.slideshowEnd = registerButtonClick( 'stopslideshow', options.$stopSlideshow );
   
   if (options.$gallery) {
     _activateGallery( $( options.$gallery ) );
-    exports.dom.galleryActivated = false;
+    ag.dom.galleryActivated = false;
   }
   
   if (options.quickSetup) {
     Switcher.slideshow();
-    prev = registerButtonClick( 'prev' );
-    next = registerButtonClick( 'next' );
-    startSlideshow = registerButtonClick( 'slideshow' );
-    stopSlideshow = registerButtonClick( 'stopslideshow' );
+    ag.dom.prev = registerButtonClick( 'prev' );
+    ag.dom.next = registerButtonClick( 'next' );
+    ag.dom.slideshowStart = registerButtonClick( 'slideshow' );
+    ag.dom.slideshowEnd = registerButtonClick( 'stopslideshow' );
   }
   
   return AG;
@@ -192,8 +193,8 @@ AG.init = function ( options ) {
 
 AG.extend = function ( callback, methodName ) {
   methodName ? (AG[methodName] = function () {
-    callback( exports );
-  }) : callback( exports );
+    callback( ag );
+  }) : callback( ag );
   
   return AG;
 };
