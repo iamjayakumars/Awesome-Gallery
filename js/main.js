9d55405b85efb1380ccd148cbd9a90c3386f833f
Daniel Desira <desiradaniel2007@hotmail.com>
2013-12-06 22:28:24 +0100

    Gallery spawning updated to reflect 0.5 changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-awesomeGallery({
-  imageSelector: '#mainPhoto',
-  imagesSelector: '.photo',
-  slideshow: function (slideshow) {
-    document.getElementById('slideshow').addEventListener('click', slideshow);
-  }
+AG.init({
+  $image: '#mainPhoto',
+  $$images: '.photo',
+  $gallery: '#gallery'
 });
