05572a73e35f1445788d3c8e88b87ab450c7eaef
Daniel Desira <desiradaniel2007@hotmail.com>
2013-12-15 14:19:37 +0100

    Xmaxy now using array forEach() method

--- js/plugins/xmaxy.js
+++ js/plugins/xmaxy.js
@@ -9,17 +9,17 @@ carol.play();
 function lightImages( images, redEven ) {
   var RED = 'rgb(255, 0, 0)',
     GREEN = 'rgb(0, 255, 0)';
-  for (var i = 0; i < images.length; i++) {
-    images[i].style.borderColor = redEven ? ( i % 2 === 0 ? RED : GREEN ) :
-          ( i % 2 === 0 ? GREEN : RED );
-  }
+  images.forEach(function ( image, index ) {
+    image.style.borderColor = redEven ? ( index % 2 === 0 ? RED : GREEN ) :
+          ( index % 2 === 0 ? GREEN : RED );
+  });
   return !redEven;
 }
 
 AG.extend(function ( $ ) {
   var redEven = true,
     images = $.dom.images;
-  Array.prototype.forEach.call( images, function ( image ) {
+  images.forEach(function ( image ) {
     image.style.borderWidth = '5px';
   });
   redEven = lightImages( images, redEven );
